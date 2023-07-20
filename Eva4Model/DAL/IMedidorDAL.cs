0ca3d8237f35b54ad08b178c8afb69848f0b7ed9
WhiskyRodrigo <rodrigo.diocares@inacapmail.cl>
2023-07-19 22:54:43 -0400

    ..

---
+++ Eva4Model/DAL/IMedidorDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Admin.DTO;
+
+namespace Admin.DAL
+{
+    public interface IMedidorDAL
+    {
+        // obtiene y devuelve la lista de medidores.
+        List<Medidor> ObtenerMedidor();   
+
+        //agrega medidor.
+        void AgregarMedidor(Medidor medidor);   
+
+    }
+}
